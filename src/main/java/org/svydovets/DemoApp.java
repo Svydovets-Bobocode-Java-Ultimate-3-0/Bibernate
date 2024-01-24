8af2a80de3428cd406078f29cc9b08c15adcf1d1
Oleksii Makieiev <alexmakeev2703@gmail.com>
2024-01-24 23:38:07 +0200

    Add the simple demo

--- src/main/java/org/svydovets/DemoApp.java
+++ src/main/java/org/svydovets/DemoApp.java
@@ -1,7 +1,19 @@
 package org.svydovets;
 
+import org.svydovets.entity.User;
+import org.svydovets.session.Session;
+import org.svydovets.session.SessionFactory;
+
 public class DemoApp {
     public static void main(String[] args) {
+        SessionFactory sessionFactory = new SessionFactory();
 
+        Session session = sessionFactory.createSession();
+
+        User user = session.findById(User.class, 1);
+
+        System.out.println(user);
+
+        session.close();
     }
 }
