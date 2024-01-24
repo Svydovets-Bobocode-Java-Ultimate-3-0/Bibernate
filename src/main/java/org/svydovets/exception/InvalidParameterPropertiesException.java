dbadaef9309c306cc2b878dba59d19fe130505da
Oleksii Makieiev <alexmakeev2703@gmail.com>
2024-01-24 23:19:31 +0200

    Add the custom exception InvalidParameterPropertiesException

---
+++ src/main/java/org/svydovets/exception/InvalidParameterPropertiesException.java
@@ -0,0 +1,8 @@
+package org.svydovets.exception;
+
+public class InvalidParameterPropertiesException extends RuntimeException {
+
+    public InvalidParameterPropertiesException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
