9506524180bc58ac6af0baba82b9993c1b324f0f
Oleksii Makieiev <alexmakeev2703@gmail.com>
2024-01-25 21:49:21 +0200

    Add the javadoc

--- src/main/java/org/svydovets/exception/InvalidParameterPropertiesException.java
+++ src/main/java/org/svydovets/exception/InvalidParameterPropertiesException.java
@@ -1,5 +1,10 @@
 package org.svydovets.exception;
 
+/**
+ * Exception class for signaling invalid parameter properties issues.
+ * This runtime exception is thrown when there are problems related to parameter properties,
+ * such as missing required properties or errors during property processing.
+ */
 public class InvalidParameterPropertiesException extends RuntimeException {
 
     public InvalidParameterPropertiesException(String message, Throwable cause) {
