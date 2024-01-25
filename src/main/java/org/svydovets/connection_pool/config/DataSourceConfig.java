436a4908feef8f069bdc4ced6180bc6642763bd8
Oleksii Makieiev <alexmakeev2703@gmail.com>
2024-01-25 21:44:59 +0200

    Add the javadoc

--- src/main/java/org/svydovets/connection_pool/config/DataSourceConfig.java
+++ src/main/java/org/svydovets/connection_pool/config/DataSourceConfig.java
@@ -4,6 +4,10 @@ import java.util.Properties;
 
 import static java.util.Objects.requireNonNull;
 
+/**
+ * Configuration class for setting up a data source.
+ * This class holds the configuration details necessary for establishing database connections.
+ */
 public class DataSourceConfig {
 
     private final String url;
@@ -11,6 +15,14 @@ public class DataSourceConfig {
     private final String password;
     private final Properties properties;
 
+    /**
+     * Constructs a new DataSourceConfig with the specified URL, username, and password.
+     *
+     * @param url      The URL of the database.
+     * @param username The username for the database login.
+     * @param password The password for the database login.
+     * @throws NullPointerException if any of the parameters are null.
+     */
     public DataSourceConfig(String url, String username, String password) {
         this.url = requireNonNull(url);
         this.username = requireNonNull(username);
@@ -18,6 +30,13 @@ public class DataSourceConfig {
         this.properties = initProperties(username, password);
     }
 
+    /**
+     * Initializes the properties for database connection with the provided username and password.
+     *
+     * @param username The database username.
+     * @param password The database password.
+     * @return A {@link Properties} object containing the database connection properties.
+     */
     public static Properties initProperties(String username, String password) {
         Properties properties = new Properties();
         properties.put("user", username);
